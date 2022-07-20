83bb0c27807218ddba6bc800b81dc3c6eae33a22
r.manan@eksad.com <Priska90>
2022-07-20 15:18:27 +0700

    prettier home3

--- lib/screen/home3.dart
+++ lib/screen/home3.dart
@@ -12,57 +12,62 @@ class Home3 extends StatelessWidget {
     return Container(
       height: screenSize.height,
       width: screenSize.width,
-      color: Color.fromARGB(255, 241, 252, 254),
+      color: const Color.fromARGB(255, 241, 252, 254),
+
       child: Row(
         children: [
           Container(
             height: 600,
             width: screenSize.width * 0.5,
-            padding: EdgeInsets.only(left: 200, top: 20),
+            padding: const EdgeInsets.only(left: 200, top: 20),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   "Explore Cloud solutions",
                   style: GoogleFonts.robotoCondensed(
-                      color: Color.fromARGB(255, 12, 66, 101),
+                      color: const Color.fromARGB(255, 12, 66, 101),
                       fontSize: 50,
                       fontWeight: FontWeight.bold),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
                 Text(
                   "for every challenge and",
                   style: GoogleFonts.robotoCondensed(
-                      color: Color.fromARGB(255, 12, 66, 101),
+                      color: const Color.fromARGB(255, 12, 66, 101),
                       fontSize: 50,
                       fontWeight: FontWeight.bold),
                 ),
                 Text(
                   "budget",
                   style: GoogleFonts.robotoCondensed(
-                      color: Color.fromARGB(255, 12, 66, 101),
+                      color: const Color.fromARGB(255, 12, 66, 101),
                       fontSize: 50,
                       fontWeight: FontWeight.bold),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 15,
                 ),
                 Text(
-                  "Instead of operating within the constraints of a single cloud vendor, a multi-cloud strategy allows you to access the cloud services from multiple cloud providers simultaneously. These cloud providers include AWS, Azure, Google Cloud Platform, or Oracle Cloud. Using the wide range of leading-edge services available from these providers on a single data set, attached to all clouds simultaneously, can boost innovation and grow your business faster than if your data was locked into a single cloud.",
+                  "Instead of operating within the constraints of a single cloud vendor,"
+                      " a multi-cloud strategy allows you to access the cloud services from multiple cloud providers simultaneously. "
+                      "These cloud providers include AWS, Azure, Google Cloud Platform, or Oracle Cloud. "
+                      "Using the wide range of leading-edge services available from these providers on a single data set, "
+                      "attached to all clouds simultaneously, can boost innovation and grow your business faster than if your data was locked into a single cloud.",
                   style: GoogleFonts.robotoCondensed(
                     color: Colors.black,
                     fontSize: 20,
                     height: 1.5,
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 30,
                 ),
                 Row(
                   children: [
-                    Container(
+                    SizedBox(
                       width: 200,
                       child: Botton_tombol(
                         title: "Learn More",
@@ -76,7 +81,8 @@ class Home3 extends StatelessWidget {
               ],
             ),
           ),
-          Container(
+
+          SizedBox(
             width: screenSize.width * 0.5,
             height: 600,
             child: ClipPath(
