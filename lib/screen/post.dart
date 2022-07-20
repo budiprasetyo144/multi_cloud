65c034b623f666efa2928f965cb9d2da19bced36
Budi Prasetyo <b.prasetyo@eksad.com>
2022-07-20 12:45:13 +0700

    multi_cloud Merapihkan package

--- lib/screen/post.dart
+++ lib/screen/post.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mcs_flutter/widget/container_post.dart';
 import 'package:mcs_flutter/text/text_post.dart';
+import 'package:msc_flutter/screen/post_body.dart';
+
