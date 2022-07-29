e01a212986b8f8934b5d115b087270802e60d3f7
arham <a.nugraha@eksad.com>
2022-07-29 22:47:07 +0700

    update multi cloud solution

--- lib/screen/home7.dart
+++ lib/screen/home7.dart
@@ -9,13 +9,12 @@ class Home7 extends StatelessWidget {
     var screenSize = MediaQuery.of(context).size;
     return Container(
       color: const Color.fromARGB(255, 227, 235, 253),
-      height: 630,
+      height: 740,
+      padding: EdgeInsets.all(10),
       width: screenSize.width,
       child: Column(
         children: [
-          const Spacer(
-            flex: 1,
-          ),
+          SizedBox(height: 10,),
           const Center(
             child: Text("Multi Cloud Solutions",
                 style: TextStyle(
@@ -23,9 +22,7 @@ class Home7 extends StatelessWidget {
                     color: kTextColor,
                     fontWeight: FontWeight.bold)),
           ),
-          const Spacer(
-            flex: 1,
-          ),
+          SizedBox(height: 20,),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -33,25 +30,49 @@ class Home7 extends StatelessWidget {
                 decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(10)),
-                padding: const EdgeInsets.all(40),
-                height: 230,
-                width: screenSize.width * 0.25,
-                child: Text(
-                  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  style: TextStyle(
-                    fontSize: 18,
-                  ),
+                padding: const EdgeInsets.all(20),
+                height: 300,
+                width: screenSize.width * 0.30,
+                child: Column(
+                  children: [
+                    Text("Effeciency Improvement",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    SizedBox(height: 5,),
+                    Text(
+                      "Cloud technology eliminates all these hassles. Cloud storage providers have the necessary infrastructure to store your data. These providers also handle the security and maintenance for you! This means you can focus more on your core business instead of buying and maintaining storage devices. Simply use the space you want and pay only for what you use.",
+                      style: TextStyle(
+                        fontSize: 18,
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Container(
-                padding: const EdgeInsets.all(40),
-                height: 230,
-                width: screenSize.width * 0.25,
-                child: Text(
-                  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  style: TextStyle(
-                    fontSize: 18,
-                  ),
+                padding: const EdgeInsets.all(20),
+                height: 300,
+                width: screenSize.width * 0.30,
+                child: Column(
+                  children: [
+                    Text("Better Scalability",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    SizedBox(height: 5,),
+                    Text(
+                      "The virtually limitless storage and compute resources available in the cloud give organizations the flexibility to scale up or down to address changing business situations. This flexibility enables a company to quickly grow to handle increased customer demand and save money by scaling down resource use when appropriate.",
+                      style: TextStyle(
+                        fontSize: 18,
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
@@ -60,32 +81,56 @@ class Home7 extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                padding: const EdgeInsets.all(40),
-                height: 230,
-                width: screenSize.width * 0.25,
-                child: Text(
-                  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  style: TextStyle(
-                    fontSize: 18,
-                  ),
+                padding: const EdgeInsets.all(20),
+                height: 300,
+                width: screenSize.width * 0.30,
+                child: Column(
+                  children: [
+                    Text("Agility & Adaptability",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    SizedBox(height: 5,),
+                    Text(
+                      "cloud services allow you to focus on important aspects of your business without having to worry about IT solutions. Relying on an external cloud computing company to manage your cloud infrastructure and host your data and applications will enable you to fully commit to achieving your business objectives.",
+                      style: TextStyle(
+                        fontSize: 18,
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Container(
-                padding: const EdgeInsets.all(40),
+                padding: const EdgeInsets.all(20),
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
-                    color: const Color.fromARGB(255, 4, 49, 173)),
-                height: 230,
-                width: screenSize.width * 0.25,
-                child: Text(
-                  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  style: TextStyle(fontSize: 18, color: Colors.white),
+                    color: Colors.white
+                ),
+                height: 300,
+                width: screenSize.width * 0.30,
+                child: Column(
+                  children: [
+                    Text("Faster Development Cycles",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    SizedBox(height: 5,),
+                    Text(
+                      "The collaboration and efficiency possible with cloud platforms will often result in faster development cycles. This enables companies to take advantage of emerging trends and more efficiently address customer demands and expectations. In today’s competitive market, the ability to bring products to market promptly can be the difference between success and failure.",
+                      style: TextStyle(
+                          fontSize: 18,
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
-          ),
-          const Spacer(
-            flex: 1,
           ),
         ],
       ),
