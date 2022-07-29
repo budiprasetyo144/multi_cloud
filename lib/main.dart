d6a52c82961d52db6afe0ffc25e998f6ac5ef7b5
Budi Prasetyo <b.prasetyo@eksad.com>
2022-07-29 09:25:11 +0700

    nambah titlebar

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Multi Cloud Solution By EKSAD",
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
