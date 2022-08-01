316570860650939a3f224dfaac28a10bc6fcdc70
r.manan@eksad.com <Priska90>
2022-08-01 09:13:34 +0700

    News: check spell text,

--- lib/text/text_body.dart
+++ lib/text/text_body.dart
@@ -1,20 +1,29 @@
-String whiteline = 'The Mission to be Establish excellent end to end IT Services to enable clients to grow their business rapidly thru high competence and professional resources.';
+String whiteline =
+    'The mission is to be an establish excellent end to end IT Services '
+    'to enable clients to grow their business rapidly '
+    'thru high competence and professional resources.';
 
-String redline = 'Our comprehensive cloud migration framework brings industrialized capabilities together with exclusive pre-configured industry-specific tools, methods, and automation across all cloud models and multiple delivery methods (IaaS, PaaS, and SaaS).';
+String redline =
+    'Our comprehensive cloud migration framework brings '
+    'industrialized capabilities together with '
+    'exclusive pre-configured industry-specific tools, methods, and automation '
+    'across all cloud models and multiple delivery methods (IaaS, PaaS, and SaaS).';
 
-String row1 = 'Eksad Providing Services High Quality Based '
-    'on Strong Experience in the industry and technology. '
-    'Building the right IT Service Solution to enable it '
-    'Partners in speeding up business development';
+String row1 =
+    'EKSAD providing services high quality based '
+    'on strong experience in the industry and technology. '
+    'Building the right IT service solution to enable its '
+    'partners in speeding up business development';
 
-String row2 = 'EKSAD provides tailored services focusing on clients’ '
+String row2 =
+    'EKSAD provides tailored services focusing on clients’ '
     'specific demands in international and domestic markets.'
     'It’s time to bridge the gap and communicate with your '
     'business across channels.';
 
-String row3 = 'Generally the detailed organization and implementation '
+String row3 =
+    'Generally the detailed organization and implementation '
     'of a complex operation. In a general business sense, logistics is '
-    ''
     'the point of consumption to meet the requirements of customers or corporations.';
 
 
