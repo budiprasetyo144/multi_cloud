48c8e0d43e627fa6a5185fc7cd30089ab174e664
Budi Prasetyo <b.prasetyo@eksad.com>
2022-08-01 09:10:19 +0700

    fix home 4

--- lib/screen/home4.dart
+++ lib/screen/home4.dart
@@ -10,126 +10,95 @@ class Home4 extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Column(
-          children: [
-            Container(
-              height: 600,
-              width: screenSize.width,
-              color: Color.fromARGB(255, 0, 36, 192),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Spacer(
-                    flex: 1,
-                  ),
-                  Container(
-                    child: Text(
-                      overflow: TextOverflow.ellipsis,
-                      "Our Partner",
-                      style: GoogleFonts.robotoCondensed(
-                          color: Colors.white,
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Container(
-                    height: screenSize.height * 0.3,
-                    width: screenSize.width * 0.8,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            Spacer(
-                              flex: 1,
-                            ),
-                            Container(
-                              width: screenSize.width * 0.3,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Image(image: AssetImage('assets/logo/alibaba.png'))
-                                ],
-                              ),
-                            ),
-                            Spacer(
-                              flex: 1,
-                            ),
-                            // BARIS 2
-                            Row(
-                              children: [
-                                Container(
-                                  width: screenSize.width * 0.3,
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Image(image: AssetImage('assets/logo/google.png'))
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Spacer(
-                              flex: 1,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Spacer(
-                    flex: 1,
-                  ),
-                  // KOLOM 2
-                  Container(
-                    height: screenSize.height * 0.20,
-                    width: screenSize.width * 0.8,
-                    child: Row(
-                      children: [
-                        Spacer(
-                          flex: 1,
-                        ),
-                        Container(
-                          width: screenSize.width * 0.3,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Image(image: AssetImage('assets/logo/aws.png',),
-                              )
-                            ],
-                          ),
-                        ),
-                        Spacer(
-                          flex: 1,
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              width: screenSize.width * 0.3,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                              Image(image: AssetImage('assets/logo/huawei.png',),
-                              )],
-                              ),
-                            )
-                          ],
-                        ),
-                        Spacer(
-                          flex: 1,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Spacer(
-                    flex: 2,
-                  ),
-                ],
+        Container(
+          height: 600,
+          width: screenSize.width,
+          //color: Color.fromARGB(255, 0, 36, 192),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Spacer(
+                flex: 1,
               ),
-            ),
-          ],
+              Container(
+                width: screenSize.width,
+                height: screenSize.height * 0.2,
+                color: Color.fromARGB(255, 0, 36, 192),
+                child: Center(
+                  child: Text(
+                    overflow: TextOverflow.ellipsis,
+                    "Our Partner",
+                    style: GoogleFonts.robotoCondensed(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Container(
+                height: screenSize.height * 0.3,
+                width: screenSize.width * 0.8,
+
+                child: Row(
+                  children: [
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Container(
+                      width: screenSize.width * 0.3,
+                      child: Image(image: AssetImage('assets/logo/alibaba.png')),
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    // BARIS 2
+                    Container(
+                      width: screenSize.width * 0.3,
+                      child: Image(image: AssetImage('assets/logo/google.png')),
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(
+                flex: 1,
+              ),
+              // KOLOM 2
+              Container(
+                height: screenSize.height * 0.20,
+                width: screenSize.width * 0.8,
+
+                child: Row(
+                  children: [
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Container(
+                      width: screenSize.width * 0.3,
+                      child: Image(image: AssetImage('assets/logo/aws.png',),
+                      ),
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Container(
+                      width: screenSize.width * 0.3,
+                      child: Image(image: AssetImage('assets/logo/huawei.png',),
+                      ),
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(
+                flex: 2,
+              ),
+            ],
+          ),
         ),
       ],
     );
