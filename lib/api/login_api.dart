d0741e354c924600ee2f7cce4a412131aca8634e
r.manan@eksad.com <Priska90>
2022-08-03 09:36:53 +0700

    partner: api & model

--- lib/api/login_api.dart
+++ lib/api/login_api.dart
@@ -2,6 +2,6 @@ import 'package:http/http.dart' as http;
 
 class LoginApi{
   Future<bool> postLogin(mail, pwd) async {
-    final response = await http.post(Uri.parse('http://10.107.250.246'),)
+    final response = await http.post(Uri.parse('http://10.107.250.246'),);
   }
 }
---
+++ lib/api/partner_api.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:mcs_flutter/model/partner_model.dart';
+
+class PartnerApi{
+
+  Future<List<dynamic>> getPartner() async {
+    var response = await http.get(
+        Uri.parse('http://10.107.250.246:8082/page/getAllPartnerByIdRole'));
+    return jsonDecode(response.body)['data'];
+  }
+  Future<bool> createPartner(name, partner) async {
+    final response = await http.post(
+        Uri.parse('http://10.107.250.246:8081/partner/savePartner'),
+        body: jsonEncode({
+          "title": name,
+          "partner": partner,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        }
+    );
+    if (response.statusCode == 200){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
