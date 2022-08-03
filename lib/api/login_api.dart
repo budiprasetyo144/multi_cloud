bb1f6ab5d3faf6e12bba59aa429b1079ac5802be
Budi Prasetyo <b.prasetyo@eksad.com>
2022-08-03 10:52:43 +0700

    button add new news

--- lib/api/login_api.dart
+++ lib/api/login_api.dart
@@ -1,7 +1,7 @@
-import 'package:http/http.dart' as http;
-
-class LoginApi{
-  Future<bool> postLogin(mail, pwd) async {
-    final response = await http.post(Uri.parse('http://10.107.250.246'),)
-  }
-}
+// import 'package:http/http.dart' as http;
+//
+// class LoginApi{
+//   Future<bool> postLogin(mail, pwd) async {
+//     final response = await http.post(Uri.parse('http://10.107.250.246'),)
+//   }
+// }
