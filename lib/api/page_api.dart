e25a2c731afd5e366ac8b6dc62dd0ab003998664
Qorri D Istajib <qorri24j@gmail.com>
2022-08-03 10:32:55 +0700

    delete model, dan setting

--- lib/api/page_api.dart
+++ lib/api/page_api.dart
@@ -1,18 +1,17 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:mcs_flutter/model/page_model.dart';
 
 class PageApi{
 
   Future<List<dynamic>> getPage() async {
     var response = await http.get(
-        Uri.parse('http://10.107.250.246:8082/page/getAllPageByIdRole'));
+        Uri.parse('http://10.107.142.52:8082/page/getAllPageByIdRole'));
     return jsonDecode(response.body)['data'];
   }
   Future<bool> createPage(name, page) async {
     final response = await http.post(
-        Uri.parse('http://10.107.250.246:8081/page/savePage'),
+        Uri.parse('http://10.107.142.52:8081/page/savePage'),
         body: jsonEncode({
           "title": name,
           "page": page
