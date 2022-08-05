36b43195ebc332e809997e5fce0d5173a0012640
rizky ramadhan <r.ramadhan@gmail.com>
2022-08-05 09:09:57 +0700

    delete news api

--- lib/api/news_api.dart
+++ lib/api/news_api.dart
@@ -2,47 +2,53 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-class NewsApi{
-  Future<bool> createNews(title,news,image) async {
+class NewsApi {
+  Future<bool> createNews(title, news, image) async {
     final response = await http.post(
         Uri.parse('http://10.3.4.231:8081/post/savePost'),
-        body: jsonEncode({
-          "title": title,
-          "post": news,
-          "image": image
-        }),
+        body: jsonEncode({"title": title, "post": news, "image": image}),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
-        }
-    );
-    if (response.statusCode == 200){
+        });
+    if (response.statusCode == 200) {
       return true;
     } else {
       return false;
     }
   }
-  Future<bool> updateNews(id,title,news,image) async {
+
+  Future<bool> updateNews(id, title, news, image) async {
     final response = await http.post(
         Uri.parse('http://10.3.4.231:8081/post/savePost'),
-        body: jsonEncode({
-          "idpost": id,
-          "title": title,
-          "post": news,
-          "image": image
-        }),
+        body: jsonEncode(
+            {"idpost": id, "title": title, "post": news, "image": image}),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
-        }
-    );
-    if (response.statusCode == 200){
+        });
+    if (response.statusCode == 200) {
       return true;
     } else {
       return false;
     }
   }
-  Future<List<dynamic>> getNews() async{
-    var response = await http.get(
-        Uri.parse('http://localhost:8082/post/getAllPostByIdRole'));
+
+  Future<List<dynamic>> getNews() async {
+    var response = await http
+        .get(Uri.parse('http://localhost:8082/post/getAllPostByIdRole'));
     return jsonDecode(response.body)['data'];
+  }
+
+  Future<bool> deleteNews(id) async {
+    final response = await http.delete(
+      Uri.parse('http://10.107.122.152:8081/partner/deletePartner/$id'),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    );
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
