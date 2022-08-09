c54e2b9a300eda086359b7d3fa0fa7c216421421
rizky ramadhan <r.ramadhan@gmail.com>
2022-08-09 15:52:54 +0700

    homee2 (how do we help ?)

--- lib/screen/homee2.dart
+++ lib/screen/homee2.dart
@@ -9,79 +9,39 @@ class Homee2 extends StatelessWidget {
     var screenSize = MediaQuery.of(context).size;
 
     return Container(
-      color: const Color.fromARGB(255, 227, 235, 253),
-      height: 400,
-      // padding: const EdgeInsets.all(10),
-      width: screenSize.width,
+      width: screenSize.width * 0.5,
+      height: 350,
+      padding: EdgeInsets.only(
+        left: screenSize.width * 0.10,
+        right: screenSize.width * 0.10,
+      ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          const SizedBox(
-            height: 10,
-          ),
-          SizedBox(
-            height: 10,
-          ),
           Container(
+            width: screenSize.width,
             child: Text('How do we help?',
                 style: TextStyle(
-                    fontSize: 45,
-                    color: kTextColor,
+                    fontSize: 30,
+                    color: Color.fromARGB(255, 12, 66, 101),
                     fontWeight: FontWeight.bold)),
           ),
-          SizedBox(
-            height: 10,
+          Container(
+            width: screenSize.width,
+            child: Text(
+                'We are a one-stop Cloud service provider for world-class Cloud services with diverse options which range from Cloud consulting services to data center migration services. Besides, we can also seamlessly scale and migrate your chosen application workloads.',
+                style: TextStyle(
+                  fontSize: 18,
+                )),
           ),
-          Row(
-            children: [
-              Container(
-                child: Text(
-                    'We are a one-stop Cloud service provider for world-class Cloud services with diverse options which range from Cloud consulting services to data center'),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                child: Text(
-                    'migration services. Besides, we can also seamlessly scale and migrate your chosen application workloads.'),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Row(
-            children: [
-              Container(
-                child: Text(
-                    'Instead of operating within the constraints of a single cloud vendor, a multi-cloud strategy allows you to access the cloud services from multiple cloud'),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                child: Text(
-                    'providers simultaneously. These cloud providers include Alibaba Cloud,  AWS, Azure, Google Cloud or Huawei Cloud. Using the wide range of leading-'),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                child: Text(
-                    'edge services available from these providers on a single data set, attached to all clouds simultaneously, can boost innovation and grow your business'),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                child: Text(
-                    'faster than if your data was locked into a single cloud.'),
-              ),
-            ],
+          Container(
+            width: screenSize.width,
+            child: Text(
+                'Instead of operating within the constraints of a single cloud vendor, a multi-cloud strategy allows you to access the cloud services from multiple cloud providers simultaneously. These cloud providers include Alibaba Cloud,  AWS, Azure, Google Cloud or Huawei Cloud. Using the wide range of leading-edge services available from these providers on a single data set, attached to all clouds simultaneously, can boost innovation and grow your business faster than if your data was locked into a single cloud.',
+                style: TextStyle(
+                  fontSize: 18,
+                )),
           ),
         ],
       ),
