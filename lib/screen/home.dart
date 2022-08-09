9939d72e139a0714921fcfb509fdfa62da62f100
rizky ramadhan <r.ramadhan@gmail.com>
2022-08-09 14:34:04 +0700

    route classs

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -12,6 +12,7 @@ import 'package:mcs_flutter/screen/home6.dart';
 import 'package:mcs_flutter/screen/home7.dart';
 import 'package:mcs_flutter/screen/home8.dart';
 import 'package:mcs_flutter/screen/home9.dart';
+import 'package:mcs_flutter/screen/homee2.dart';
 import 'package:mcs_flutter/widget/botton.dart';
 import 'package:mcs_flutter/widget/whatsapp.dart';
 
@@ -30,14 +31,16 @@ class _HomePageState extends State<HomePage> {
   PageController controller = PageController();
   void _scrollToIndex2(int index) {
     controller.animateToPage(index,
-        duration: const Duration(seconds: 2), curve: Curves.fastLinearToSlowEaseIn);
+        duration: const Duration(seconds: 2),
+        curve: Curves.fastLinearToSlowEaseIn);
   }
 
   //   Untuk pake LIST per height container
   ScrollController controller2 = ScrollController();
   void _scrollToIndex(double index) {
     controller2.animateTo(index,
-        duration: const Duration(seconds: 1), curve: Curves.fastLinearToSlowEaseIn);
+        duration: const Duration(seconds: 1),
+        curve: Curves.fastLinearToSlowEaseIn);
   }
 
   int selectedIndex = 0;
@@ -56,28 +59,30 @@ class _HomePageState extends State<HomePage> {
         controller: controller2,
         children: [
           const Home(),
+          Homee2(),
           Home2(screenSize: screenSize),
           Home3(
             isi: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  primary: kPrimaryColor),
+              onPressed: () {
+                _scrollToIndex(5900);
+              },
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                child: const Text(
+                  'Learn More',
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
                 ),
-                primary: kPrimaryColor),
-            onPressed: () {
-              _scrollToIndex(5900);
-            },
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              child: const Text(
-                'Learn More',
-                style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
               ),
             ),
-          ),),
+          ),
           const Home4(),
           const Home5(),
           const Home6(),
@@ -112,147 +117,139 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-
-
-
   PreferredSize AppbarLarge(Size screenSize) {
     return PreferredSize(
-            preferredSize: Size(screenSize.width, 100),
-            child: Container(
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                      color: Colors.grey,
-                      width: 0.5,
-                      style: BorderStyle.solid),
-                ),
-              ),
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: screenSize.width * 0.1,
-                  ),
-                  Container(
-                    padding:
-                        const EdgeInsets.only(left: 30, top: 20, bottom: 20),
-                    child: Image.asset("assets/logo/multicloudsolution.jpg"),
-                  ),
-                  SizedBox(
-                    width: screenSize.width * 0.2,
-                  ),
-                  Row(
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          _scrollToIndex(0);
-                        },
-                        child: const Text(
-                          "Home",
-                          style: TextStyle(
-                              fontSize: 17,),
-                        ),
-                        style: Color_button_appbar(),
-                      ),
-                      SizedBox(
-                        width: screenSize.width * 0.03,
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          _scrollToIndex(3150); //// POSISI ABOUT US
-                        },
-                        child: const Text(
-                          "About us",
-                          style: TextStyle(
-                              fontSize: 17,
-                              ),
-                        ),
-                        style: Color_button_appbar(),
-                      ),
-                      SizedBox(
-                        width: screenSize.width * 0.03,
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          _scrollToIndex(1230); //// POSISI SOLUTION
-                        },
-                        child: const Text(
-                          "Solution",
-                          style: TextStyle(
-                              fontSize: 17,
-                             ),
-                        ),
-                        style: Color_button_appbar(),
-                      ),
-                      SizedBox(
-                        width: screenSize.width * 0.03,
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          _scrollToIndex(5900); //// POSISI CONTACT US
-                        },
-                        child: const Text(
-                          "Contact Us",
-                          style: TextStyle(
-                              fontSize: 17,
-                          ),
-                        ),
-                        style: Color_button_appbar(),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    width: screenSize.width * 0.08,
-                  ),
-                  SizedBox(
-                    height: 50,
-                    width: 200,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: "Search",
-                        suffixIcon: const Icon(Icons.search),
-                        enabledBorder: OutlineInputBorder(
-
-                          borderSide:
-                              const BorderSide(width: 1, color: Colors.grey),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide:
-                              const BorderSide(width: 1, color: Colors.grey),
-                        ),
-                      ),
+      preferredSize: Size(screenSize.width, 100),
+      child: Container(
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+                color: Colors.grey, width: 0.5, style: BorderStyle.solid),
+          ),
+        ),
+        child: Row(
+          children: [
+            SizedBox(
+              width: screenSize.width * 0.1,
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 30, top: 20, bottom: 20),
+              child: Image.asset("assets/logo/multicloudsolution.jpg"),
+            ),
+            SizedBox(
+              width: screenSize.width * 0.2,
+            ),
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    _scrollToIndex(0);
+                  },
+                  child: const Text(
+                    "Home",
+                    style: TextStyle(
+                      fontSize: 17,
                     ),
                   ),
-                  const Spacer(),
-                  // IconButton(
-                  //     onPressed: () {
-                  //       Navigator.push(
-                  //         context,
-                  //         MaterialPageRoute(
-                  //           builder: (context) => const Dashboard(),
-                  //         ),
-                  //       );
-                  //     },
-                  //     icon: const Icon(
-                  //       Icons.brightness_low_sharp,
-                  //       color: Colors.white,
-                  //     ))
-                ],
+                  style: Color_button_appbar(),
+                ),
+                SizedBox(
+                  width: screenSize.width * 0.03,
+                ),
+                TextButton(
+                  onPressed: () {
+                    _scrollToIndex(3150); //// POSISI ABOUT US
+                  },
+                  child: const Text(
+                    "About us",
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  style: Color_button_appbar(),
+                ),
+                SizedBox(
+                  width: screenSize.width * 0.03,
+                ),
+                TextButton(
+                  onPressed: () {
+                    _scrollToIndex(1230); //// POSISI SOLUTION
+                  },
+                  child: const Text(
+                    "Solution",
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  style: Color_button_appbar(),
+                ),
+                SizedBox(
+                  width: screenSize.width * 0.03,
+                ),
+                TextButton(
+                  onPressed: () {
+                    _scrollToIndex(5900); //// POSISI CONTACT US
+                  },
+                  child: const Text(
+                    "Contact Us",
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  style: Color_button_appbar(),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: screenSize.width * 0.08,
+            ),
+            SizedBox(
+              height: 50,
+              width: 200,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Search",
+                  suffixIcon: const Icon(Icons.search),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(width: 1, color: Colors.grey),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(width: 1, color: Colors.grey),
+                  ),
+                ),
               ),
             ),
-          );
+            const Spacer(),
+            // IconButton(
+            //     onPressed: () {
+            //       Navigator.push(
+            //         context,
+            //         MaterialPageRoute(
+            //           builder: (context) => const Dashboard(),
+            //         ),
+            //       );
+            //     },
+            //     icon: const Icon(
+            //       Icons.brightness_low_sharp,
+            //       color: Colors.white,
+            //     ))
+          ],
+        ),
+      ),
+    );
   }
 
   ButtonStyle Color_button_appbar() {
     return ButtonStyle(
-                        foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                                (Set<MaterialState> states) {
-                              if (states.contains(MaterialState.hovered))
-                                // ignore: curly_braces_in_flow_control_structures
-                                return Colors.blue;
-                              return Colors.black; // null throus error in flutter 2.2+.
-                            }),
-                      );
+      foregroundColor:
+          MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
+        if (states.contains(MaterialState.hovered))
+          // ignore: curly_braces_in_flow_control_structures
+          return Colors.blue;
+        return Colors.black; // null throus error in flutter 2.2+.
+      }),
+    );
   }
 }
 
@@ -271,54 +268,80 @@ class _HomeState extends State<Home> {
       children: [
         Container(
           width: screenSize.width,
-          height: screenSize.height*0.7,
-          decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/images/home1.jpg",),fit: BoxFit.fill)),
+          height: screenSize.height * 0.7,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                    "assets/images/home1.jpg",
+                  ),
+                  fit: BoxFit.fill)),
         ),
         Container(
-          width: screenSize.width*0.5,
-          height: screenSize.height*0.65,
-          padding: EdgeInsets.only(left: screenSize.width * 0.1, top: screenSize.height * 0.02),
-
+          width: screenSize.width * 0.5,
+          height: screenSize.height * 0.65,
+          padding: EdgeInsets.only(
+              left: screenSize.width * 0.1, top: screenSize.height * 0.02),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
                 child: Text(
-                  'Cloud Consulting Services.',style: TextStyle(fontSize: 50,fontWeight: FontWeight.bold,color: Colors.white),overflow: TextOverflow.ellipsis,),
+                  'Cloud Consulting Services.',
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
               Container(
-                width: screenSize.width*0.5,
-                height: screenSize.height*0.17,
-                child: Text('We helps organizations transform their IT environment '
+                width: screenSize.width * 0.5,
+                height: screenSize.height * 0.17,
+                child: Text(
+                    'We helps organizations transform their IT environment '
                     'with cost-effective Cloud services. Our team of experts delivers '
                     'cutting-edge Cloud computing consulting services that help companies '
-                    'boost their productivity to new heights.',style: TextStyle(fontSize:16,letterSpacing: 2,height:1.6,fontWeight: FontWeight.w400,color: Colors.white)),
+                    'boost their productivity to new heights.',
+                    style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 2,
+                        height: 1.6,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white)),
               ),
               Container(
-                width: screenSize.width*0.5,
-                height: screenSize.height*0.21,
-                child: Text('Our Cloud managed services make it possible for organizations '
+                width: screenSize.width * 0.5,
+                height: screenSize.height * 0.21,
+                child: Text(
+                    'Our Cloud managed services make it possible for organizations '
                     'to reduce their overheads and better productivity, in addition to lowering '
                     'costs and reducing time-to-market. We assist customers with our on-demand '
                     'enterprise Cloud solutions, tools, and applications such as servers, storage,'
-                    ' databases, networking, software, and apps, among others.',style: TextStyle(fontSize:16,letterSpacing: 2,height:1.6,fontWeight: FontWeight.w400,color: Colors.white)),
+                    ' databases, networking, software, and apps, among others.',
+                    style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 2,
+                        height: 1.6,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white)),
               ),
               Container(
-                width: screenSize.width*0.15,
-                  height: screenSize.height*0.08,
-                  child: ElevatedButton(onPressed: (){}, child: Text('CONTACT US',style: TextStyle(fontSize: 19,letterSpacing: 2),
-                  ),
-                    style: ElevatedButton.styleFrom(primary: Color.fromARGB(
-                        255, 29, 59, 189),)
-                  ),
+                width: screenSize.width * 0.15,
+                height: screenSize.height * 0.08,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'CONTACT US',
+                      style: TextStyle(fontSize: 19, letterSpacing: 2),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color.fromARGB(255, 29, 59, 189),
+                    )),
               )
             ],
           ),
-
-
-          ),
-
+        ),
       ],
     );
   }
