474668b5e141f2cc7875839e5107ad3a5e371209
r.manan@eksad.com <Priska90>
2022-08-18 16:46:18 +0700

    tes akun git

--- lib/screen/footer.dart
+++ lib/screen/footer.dart
@@ -45,7 +45,7 @@ class Footer extends StatelessWidget {
                                         'assets/logo/logo-eksad.png'),
                                     fit: BoxFit.fill)),
                           ),
-                           Spacer(),
+                          const Spacer(),
                           Container(
                             width: screenSize.width * 0.17,
                             height: screenSize.height * 0.04,
